4efedfb00bd5f96f6bd411640d3ed97a1362c9b4
Steffen Schotthöfer <steffen.schotthoefer@kit.edu>
2023-07-31 10:56:04 +0200

    added henyey greenstein kernel. Checked that 1 is a collision invariant

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import numpy as np
 from src.collision_operator_1D import CollisionOperator1D
+from src.collision_operator_HG_1D import CollisionOperatorHG1D
 
 import matplotlib.pyplot as plt
 
+
 def main():
     integration_order = 14
-    Q = CollisionOperator1D(integration_order)
+    g = 0.5  # g\in (-1,1)
+    Q = CollisionOperatorHG1D(integrator_order=integration_order, anisotropy_param=0.99)
+    # Q = CollisionOperator1D(integrator_order=integration_order)
 
     sensor_pts = Q.get_quad_pts()
 
-    f_vq  = np.sin(np.pi*sensor_pts) # get an example function evaluated at sensor pts
+    f_vq = np.sin(np.pi * sensor_pts) + 1  # get an example function evaluated at sensor pts
 
     f_out = Q.evaluate_Q(f_vq)
     plt.plot(sensor_pts, f_vq, '-k')
@@ -18,6 +22,7 @@ def main():
     plt.show()
 
     return 0
+
 
 if __name__ == '__main__':
     main()
