c85be9531ef6707247d525da16a85484ce044f74
Steffen <steffen.schotthoefer@kit.edu>
2023-10-17 07:25:03 -0400

    addons from summer

--- main.py
+++ main.py
@@ -3,21 +3,37 @@ from src.collision_operator_1D import CollisionOperator1D
 
 import matplotlib.pyplot as plt
 
+
 def main():
-    integration_order = 14
+    integration_order = 10
     Q = CollisionOperator1D(integration_order)
 
     sensor_pts = Q.get_quad_pts()
 
-    f_vq  = np.sin(np.pi*sensor_pts) # get an example function evaluated at sensor pts
+    tau = 0.1
+    f_vq = sensor_pts  # np.sin(    np.pi * sensor_pts)  # np.exp(-(sensor_pts ** 2) / tau)  # get an example function evaluated at sensor pts
 
-    f_out = Q.evaluate_Q(f_vq)
-    plt.plot(sensor_pts, f_vq, '-k')
-    plt.plot(sensor_pts, f_out, '^-r')
-    plt.legend(["f", "Q(f)"])
-    plt.show()
+    f_out = Q.evaluate_Q(f_vq, g=0.9)
+    f_out2 = Q.evaluate_Q(f_vq, g=0.5)
+    f_out3 = Q.evaluate_Q(f_vq, g=0.2)
+    f_out4 = Q.evaluate_Q(f_vq, g=-0.2)
+    f_out5 = Q.evaluate_Q(f_vq, g=-0.5)
+    f_out6 = Q.evaluate_Q(f_vq, g=-0.9)
+
+    gs = np.linspace(-1, 1, 10)
+
+    fig, ax = plt.subplots()
+    ax.plot(sensor_pts, f_vq, '-k', label="f")
+    for i in range(10):
+        f_out = Q.evaluate_Q(f_vq, g=gs[i])
+        ax.plot(sensor_pts, f_out, label="Q(f)," + str(gs[i]))
+    ax.legend()
+    fig.show()
+    plt.savefig("collision.png")
+    # plt.show()
 
     return 0
+
 
 if __name__ == '__main__':
     main()
--- src/collision_operator_1D.py
+++ src/collision_operator_1D.py
@@ -1,9 +1,10 @@
 import numpy as np
 from numpy.polynomial.legendre import leggauss
 
+
 class CollisionOperator1D():
 
-    def __init__(self, integrator_order:int):
+    def __init__(self, integrator_order: int):
         """
 
         :param n_q: order of the integration rule for the Gauss Legendre Quadrature
@@ -15,22 +16,20 @@ class CollisionOperator1D():
         self.integrator_order = integrator_order
         self.n_q = quad_weights.size
 
-        self.kernel = 0.5*np.ones(self.n_q ) # placeholder for other kernels: Isotropic kernel
-
-    def get_quad_pts(self)-> np.array:
+    def get_quad_pts(self) -> np.array:
         return self.v_q
 
-    def get_quad_weights(self)-> np.array:
+    def get_quad_weights(self) -> np.array:
         return self.w_q
 
-    def qGaussLegendre1D(self,order: int)-> tuple:
+    def qGaussLegendre1D(self, order: int) -> tuple:
         """
         order: order of quadrature
         returns: [quad_pts, quad_weights] : quadrature points and weights (use to get sensor points)
         """
         return leggauss(order)
 
-    def integrate(self,integrand, weights)-> np.array:
+    def integrate(self, integrand, weights) -> np.array:
         """
         params: weights = quadweights vector (at quadpoints) (dim = nq)
                 integrand = integrand vector, evaluated at quadpts (dim = vectorlen x nq)
@@ -38,15 +37,46 @@ class CollisionOperator1D():
         """
         return np.dot(integrand, weights)
 
-    def evaluate_Q(self,f_in:np.array)-> np.array:
+    def evaluate_Q(self, f_in: np.array, g: float) -> np.array:
         """
         (only for an isotropic collision kernel right now)
 
         :param f_in: function evaluated at quadrature points (use get_quad_pts to get them)
         :return: Q(f) using the specified kollision kernel
         """
+        kernel = self.calc_h_g_scattering_kernel(self.v_q, self.v_q, g=g)
+        #        f_out = kernel.dot(f_in * self.w_q) - kernel.dot(self.w_q) * f_in
 
-        f_out = np.dot(self.kernel*f_in,self.w_q) - f_in
+        t2 = 0
+        for i in range(len(f_in)):
+            t2 += self.henyey_greenstein_kernel(v=0.3, v_prime=self.v_q[i], g=g) * self.w_q[i]
 
+        t = kernel.dot(self.w_q)
+        f_out = np.zeros(len(f_in))
+        for i in range(len(f_in)):
+            for j in range(len(f_in)):
+                f_out[i] += kernel[i, j] * f_in[j] * self.w_q[j]
+            f_out[i] -= np.dot()
 
         return f_out
+
+    @staticmethod
+    def calc_h_g_scattering_kernel(v: np.ndarray, v_prime: np.ndarray, g: float) -> np.ndarray:
+        """
+        :param v: velocity variable
+        :param v_prime: integration velocity variable
+        :param g: velocity variable
+        :return: scattering kerenel for each
+        """
+        return (1 - g ** 2) / (4 * np.pi * np.sqrt(1 - 2 * g * np.outer(v, v_prime) + g ** 2) ** 3)
+
+    # def henyey_greenstein_kernel(self, mu):
+    @staticmethod
+    def henyey_greenstein_kernel(v: float, v_prime: float, g: float) -> np.ndarray:
+        """
+        :param v: velocity variable
+        :param v_prime: integration velocity variable
+        :param g: velocity variable
+        :return: scattering kerenel for each
+        """
+        return (1 - g ** 2) / (np.sqrt(2) * np.sqrt(1 - 2 * g * v * v_prime + g ** 2))
