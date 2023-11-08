b73db13c938392c6a1e8bc4e46bd7d64993684e2
jaeyong <jaeyong@kias.re.kr>
2023-11-08 18:40:38 +0900

    Consolidate our ideas for property enforcement and integrate them into train.py for clear organization.

--- model/deeponet.py
+++ model/deeponet.py
@@ -1,63 +1,122 @@
-import torch.nn as nn
 import torch
-    
-class deeponet(nn.Module):
-    def __init__(self, depth_trunk, width_trunk, depth_branch, width_branch, act, num_basis, num_sensor, output_d_in, output_d_out, fix_bias=False, val_bias=0):
-        super(deeponet, self).__init__()
-        self.output_d_out=output_d_out
-        self.n_basis=num_basis
-        self.num_sensor=num_sensor
-        self.fix_bias=fix_bias
-        if self.fix_bias:
-            self.val_bias = val_bias
-        
-        ##trunk net
-        if act=='tanh':
-            self.activation=nn.Tanh()
-        elif act=='prelu':
-            self.activation=nn.PReLU()
-        elif act=='relu':
-            self.activation=nn.ReLU()
-        else:
-            print('activation error!!')
-            
-        if width_trunk!=width_branch:
-            print('width no same error!!')
-            
-        self.trunk_list = []
-        self.trunk_list.append(nn.Linear(output_d_in,width_trunk))
-        self.trunk_list.append(self.activation)
-        for i in range(depth_trunk-1):
-            self.trunk_list.append(nn.Linear(width_trunk, width_trunk))
-            self.trunk_list.append(self.activation)
-        self.trunk_list.append(nn.Linear(width_trunk, self.output_d_out*self.n_basis))
-        self.trunk_list.append(self.activation)
-        self.trunk_list = nn.Sequential(*self.trunk_list)
-        
-        ##branch net
-        self.branch_list = []
-        self.branch_list.append(nn.Linear(self.num_sensor,width_branch))
-        self.branch_list.append(self.activation)
-        for i in range(depth_branch-1):
-            self.branch_list.append(nn.Linear(width_branch, width_branch))
-            self.branch_list.append(self.activation)
+from torch import nn
 
-        self.branch_list.append(nn.Linear(width_branch, self.output_d_out*(self.n_basis+1)))
+
+
+class deeponet(nn.Module):
+    def __init__(self, branch_hidden, trunk_hidden, act, output_d_out, use_bias, use_gram, quad_w=None, size_domain=None):
+        super(deeponet, self).__init__()
+        self.branch_hidden=branch_hidden
+        self.branch_num_layer=len(branch_hidden)
+        self.trunk_hidden=trunk_hidden
+        self.trunk_num_layer=len(trunk_hidden)
+        self.output_d_out=output_d_out
+        self.use_bias=use_bias
+        # use_bias = False or 'vanila' or 'depend'
+        if self.use_bias=='vanila':
+            self.bias = nn.Parameter(torch.zeros(self.output_d_out))
+
+        if trunk_hidden[-1]==branch_hidden[-1]:
+            self.n_basis=branch_hidden[-1]
+        self.use_gram=use_gram
+        self.quad_w=quad_w
+        self.size_domain=size_domain
+
+        # Type of activation function
+        if act=='tanh':
+            self.act=nn.Tanh()
+        elif act=='prelu':
+            self.act=nn.PReLU()
+        elif act=='relu':
+            self.act=nn.ReLU()
+        elif act=='gelu':
+            self.act=nn.GELU()
+        else:
+            print('Error!!! No activation!')
+
+        
+        self.branch_list = []
+        for i in range(self.branch_num_layer-1):
+            # hidden layers
+            if i!=self.branch_num_layer-2:
+                self.branch_list.append(nn.Linear(branch_hidden[i], branch_hidden[i+1]))
+                self.branch_list.append(self.act)
+            # final layers without final activation
+            else:
+                self.branch_list.append(nn.Linear(branch_hidden[i], self.output_d_out*branch_hidden[i+1]))
         self.branch_list = nn.Sequential(*self.branch_list)
         
+        self.trunk_list = []
+        for i in range(self.trunk_num_layer-1):
+            # hidden layers
+            if i!=self.trunk_num_layer-2:
+                self.trunk_list.append(nn.Linear(trunk_hidden[i], trunk_hidden[i+1])) 
+                self.trunk_list.append(self.act)
+            # final layers with final activation
+            else:
+                self.trunk_list.append(nn.Linear(trunk_hidden[i], self.output_d_out*trunk_hidden[i+1]))
+                self.trunk_list.append(self.act)
+        self.trunk_list = nn.Sequential(*self.trunk_list)
+    
+    def gram_schmidt(self, vv, quad_w):
+        ## vv = (N , d)
+        ## N basis functions at d quad pts values
+        def projection(u, v):
+            #print(u.shape, v.shape)
+            return (v * u*quad_w).sum() / (u * u*quad_w).sum() * u
 
-        
-    def forward(self, data_grid, data_sensor):
-        
+        nk = vv.size(0)
+        uu = torch.zeros_like(vv, device=vv.device)
+        uu[0] = vv[0].clone()
+        for k in range(1, nk):
+            vk = vv[k].clone()
+            uk = 0
+            for j in range(0, k):
+                uj = uu[j].clone()
+                uk = uk + projection(uj, vk)
+            uu[k] = vk - uk
+        for k in range(nk):
+            uk = uu[k].clone()
+            uu[k] = uk / ((uk * uk*quad_w).sum()**0.5)
+        return uu
+    
+    def get_basis(self, data_grid, add_one_basis=False):
+        if self.use_gram:
+            # Use gram
+            add_basis=torch.cat((torch.ones(1,self.branch_hidden[0]).to(data_grid.device),self.trunk_list(data_grid).transpose(0,1)))
+            basis=self.gram_schmidt(add_basis, self.quad_w.to(data_grid.device))[1:].transpose(0,1)
+        else:
+            # No use gram
+            basis=self.trunk_list(data_grid)
+        if add_one_basis:
+            basis=torch.cat((torch.ones(self.branch_hidden[0],1).to(data_grid.device),self.trunk_list(data_grid)), dim=1)
+        return basis
+
+    def forward(self, data_sensor, data_grid):
+        # Input data_sensor shape : B_sensor x num_sensor
+        # Input data_grid shape : B_sensor x d_in
         B_sensor=data_sensor.shape[0]
         B_grid=data_grid.shape[0]
-        coeffs=self.branch_list(data_sensor).reshape(B_sensor,self.output_d_out,1,self.n_basis+1).repeat(1,1,B_grid,1)
-        coeff=coeffs[...,:-1]
-        bias=coeffs[...,-1]
-        if self.fix_bias:
-            bias=bias*self.val_bias
-        basis=self.trunk_list(data_grid).reshape(1,self.output_d_out,B_grid,self.n_basis).repeat(B_sensor,1,1,1)
         
-        y=torch.einsum("bijk,bijk->bij", coeff, basis)+bias
+        # Coefficient in branch net
+        coeff=self.branch_list(data_sensor).reshape(B_sensor,self.output_d_out,1,self.n_basis).repeat(1,1,B_grid,1)
+        
+        # Basis in trunk net
+        # if self.use_gram:
+        #     # Use gram
+        #     add_basis=torch.cat((torch.ones(1,data_sensor.shape[-1]).to(data_grid.device),self.trunk_list(data_grid).transpose(0,1)))
+        #     basis=self.gram_schmidt(add_basis, self.quad_w.to(data_grid.device))[1:].transpose(0,1).reshape(1,self.output_d_out,B_grid,self.n_basis).repeat(B_sensor,1,1,1)
+        # else:
+        #     # No use gram
+        #     basis=self.trunk_list(data_grid).reshape(1,self.output_d_out,B_grid,self.n_basis).repeat(B_sensor,1,1,1)
+        basis=self.get_basis(data_grid).reshape(1,self.output_d_out,B_grid,self.n_basis).repeat(B_sensor,1,1,1)
 
+        # Set bias to enforce properties of collision operator (bias is a coefficient for 1-constant bases)
+        if self.use_bias=='depend':
+            self.bias = -(1/self.size_domain)*torch.sum(self.quad_w.to(data_grid.device).reshape(-1,1)*coeff*basis, dim=[-1,-2]).reshape(B_sensor,self.output_d_out,1).repeat(1,1,B_grid)
+
+        # y shape : B_sensor x output_d_out x B_grid
+        y=torch.einsum("bijk,bijk->bij", coeff, basis)
+        if self.use_bias!=False:
+            y+=self.bias
         return y
--- train.py
+++ train.py
@@ -1,27 +1,35 @@
-#python3 train.py 3_8_3_8 --model deeponet --seed 0 --gpu 1 --epochs 100000 --lambda 0 --d_t 3 --w_t 8 --d_b 3 --w_b 8 --act tanh --n_basis 8
+### Train command examples
+## vanila DeepONet wo/ bias
+# python3 train.py 1d_3_8_3_8_wo_bias --seed 0 --gpu 0 --dimension 1 --data_file toy --integration_order 100 --model deeponet --branch_hidden 100 8 8 8 --trunk_hidden 1 8 8 8 --use_bias no --epochs 100000 --lambda 0
 
-#python3 train.py 3_8_3_8_enforce --model deeponet --seed 0 --gpu 2 --epochs 100000 --lambda 0.1 --d_t 3 --w_t 8 --d_b 3 --w_b 8 --act tanh --n_basis 8 --fix_bias
+## vanila DeepONet w/ bias
+# python3 train.py 1d_3_8_3_8_w_bias --seed 0 --gpu 0 --dimension 1 --data_file toy --integration_order 100 --model deeponet --branch_hidden 100 8 8 8 --trunk_hidden 1 8 8 8 --use_bias vanila --epochs 100000 --lambda 0
 
+## (soft constraint) DeepONet with additional orthogonal loss
+# python3 train.py 1d_3_8_3_8_soft_lamb01 --seed 0 --gpu 1 --dimension 1 --data_file toy --integration_order 100 --model deeponet --branch_hidden 100 8 8 8 --trunk_hidden 1 8 8 8 --use_bias vanila --epochs 100000 --lambda 0.1
+
+## (Hard constraint) DeepONet with gram schmidt for basis
+# python3 train.py 1d_3_8_3_8_hard_gram --seed 0 --gpu 2 --dimension 1 --data_file toy --integration_order 100 --model deeponet --branch_hidden 100 8 8 8 --trunk_hidden 1 8 8 8 --use_bias no --use_gram --epochs 100000 --lambda 0
+
+## (Hard constraint) DeepONet with special bias (depends on input function)
+# python3 train.py 1d_3_8_3_8_hard_special --seed 0 --gpu 3 --dimension 1 --data_file toy --integration_order 100 --model deeponet --branch_hidden 100 8 8 8 --trunk_hidden 1 8 8 8 --use_bias depend --epochs 100000 --lambda 0
 
 from model.deeponet import *
 from utils import *
 import numpy as np
 from src.collision_operator_1D import CollisionOperator1D
-import matplotlib.pyplot as plt
+from src.collision_operator_3D import CollisionOperator3D
 import os
-from os import path
 import sys
 from tqdm import tqdm
-from math import pi
 import argparse
 import random
 import torch
 from torch.utils.data import TensorDataset, DataLoader
-from torch.autograd import Variable
 import torch.nn as nn
 
 
-torch.set_default_dtype(torch.float64)
+#torch.set_default_dtype(torch.float64)
 
 
 ## ARGS
@@ -29,26 +37,30 @@ parser = argparse.ArgumentParser(description = 'Put your hyperparameters')
 
 ### Setting
 parser.add_argument('name', type=str, help='experiments name')
-parser.add_argument('--model', default='deeponet', type=str)
 parser.add_argument("--seed", type=int, default=0)
 parser.add_argument("--use_squeue", action='store_true')
 parser.add_argument("--gpu", type=int, default=0)
 
+
+### Data
+parser.add_argument("--dimension", type=int, default=1)
+parser.add_argument("--data_file", default='toy', type=str)
+parser.add_argument("--integration_order", type=int)
+
+### Model
+parser.add_argument('--model', default='deeponet', type=str)
+parser.add_argument('--branch_hidden', default=[100,8,8,8], nargs='+', type=int, help='branch network')
+parser.add_argument('--trunk_hidden', default=[1,8,8,8], nargs='+', type=int, help='trunk network')
+parser.add_argument('--act', default='tanh', type=str, help='activation function')
+parser.add_argument('--d_out', default=1, type=int, help='dimension of output for target function')
+parser.add_argument("--use_bias", type=str, choices=['no','vanila', 'depend'], required=True)
+parser.add_argument("--use_gram", action='store_true')
 
 ### Train parameters
 parser.add_argument('--batch_size', default=0, type=int, help = 'batch size for train data (0 is full batch)')
 parser.add_argument('--epochs', default=100000, type=int, help = 'Number of Epochs')
 parser.add_argument('--lr', default=1e-3, type=float, help='learning rate')
 parser.add_argument('--lambda', default=0, type=float, help='Loss weight for orthogonality')
-parser.add_argument('--d_t', default=2, type=int, help='depth of target network (except basis)')
-parser.add_argument('--w_t', default=20, type=int, help='width of target network (except basis)')
-parser.add_argument('--d_b', default=2, type=int, help='depth of branch network (except basis)')
-parser.add_argument('--w_b', default=20, type=int, help='width of branch network (except basis)')
-parser.add_argument('--act', default='tanh', type=str, help='activation function')
-parser.add_argument('--n_basis', default=20, type=int, help='number of basis') 
-parser.add_argument('--d_in', default=1, type=int, help='dimension of input for target function')
-parser.add_argument('--d_out', default=1, type=int, help='dimension of output for target function')
-parser.add_argument("--fix_bias", action='store_true')
 
 
 
@@ -57,6 +69,7 @@ gparams = args.__dict__
 
 
 
+### Setting
 ## Random seed
 random_seed=gparams['seed']
 torch.manual_seed(random_seed)
@@ -72,29 +85,31 @@ if not gparams['use_squeue']:
     print("Is available to use cuda? : ",use_cuda)
     if use_cuda:
         print("-> GPU number ",gpu_id)
-    
+
 ## File name and path
 NAME=gparams['name']
-PATH = 'results/{}/'.format(gparams['model'])
-if not os.path.exists(PATH):    
-    os.mkdir(PATH)
-PATH = os.path.join(PATH, NAME)
+if not os.path.exists('results'):    
+    os.mkdir('results')
+PATH = os.path.join('results', NAME)
 os.mkdir(PATH)
 torch.save(args, os.path.join(PATH, 'args.bin'))
 
-## Load data
-train_data=np.load('data/train_data.npz')
-test_data=np.load('data/test_data.npz')
 
-train_data_f, train_data_Q = torch.DoubleTensor(train_data['data_f']), torch.DoubleTensor(train_data['data_Q'])
-test_data_f, test_data_Q = torch.DoubleTensor(test_data['data_f']), torch.DoubleTensor(test_data['data_Q'])
+
+### Data
+## Load data
+train_data=np.load('data/'+gparams['data_file']+'_train_data.npz')
+test_data=np.load('data/'+gparams['data_file']+'_test_data.npz')
+
+train_data_f, train_data_Q = torch.FloatTensor(train_data['data_f']), torch.FloatTensor(train_data['data_Q'])
+test_data_f, test_data_Q = torch.FloatTensor(test_data['data_f']), torch.FloatTensor(test_data['data_Q'])
 
 resol=train_data_f.shape[-1]
 num_train=train_data_f.shape[0]
 num_test=test_data_f.shape[0]
 
 if gparams['batch_size']==0:
-    batch_size_train=train_data_f.shape[0]
+    batch_size_train=num_train
 else:
     batch_size_train = gparams['batch_size']
 
@@ -102,29 +117,47 @@ else:
 dataset=TensorDataset(train_data_f.unsqueeze(1),train_data_Q)
 train_dataloader = DataLoader(dataset, batch_size=batch_size_train, shuffle=True)
 
-# Test data
+## Test data
 batch_size_test = num_test
 dataset=TensorDataset(test_data_f.unsqueeze(1),test_data_Q)
 test_dataloader = DataLoader(dataset, batch_size=batch_size_test, shuffle=False)
 
+## guad pts and weights
+integration_order=gparams['integration_order']
+if gparams['dimension']==1:
+    Q = CollisionOperator1D(integration_order)
+    grid = torch.FloatTensor(Q.get_quad_pts()).reshape(-1,1).cuda()
+    quad_w = torch.FloatTensor(Q.get_quad_weights()).cuda()
+    size_domain=2
+elif gparams['dimension']==3:
+    Q = CollisionOperator3D(integration_order)
+    grid = torch.FloatTensor(Q.get_quad_pts()).reshape(-1,3).cuda()
+    quad_w = torch.FloatTensor(Q.get_quad_weights()).cuda()
+    size_domain=2
 
 
 
-d_t=gparams['d_t']
-w_t=gparams['w_t']
-d_b=gparams['d_b']
-w_b=gparams['w_b']
+### Model
+branch_hidden=gparams['branch_hidden']
+trunk_hidden=gparams['trunk_hidden']
 act=gparams['act']
-n_basis=gparams['n_basis']
-n_sensor=resol
-output_d_in=gparams['d_in']
 output_d_out=gparams['d_out']
-
+if gparams['use_bias']=='no':
+    use_bias=False
+else:
+    use_bias=gparams['use_bias']    
+if gparams['use_bias']=='no' and gparams['use_gram']==False:
+    n_basis=trunk_hidden[-1]
+else:
+    n_basis=trunk_hidden[-1]+1
+use_gram=gparams['use_gram']
 ## Set model
 if gparams['model']=='deeponet':
-    DeepONet=deeponet(d_t, w_t, d_b, w_b, act, n_basis, n_sensor, output_d_in, output_d_in, fix_bias=gparams['fix_bias']).cuda()
+    DeepONet=deeponet(branch_hidden, trunk_hidden, act, output_d_out, use_bias, use_gram, quad_w, size_domain).cuda()
+print(DeepONet)
 
-## Train model
+
+### Train parameters
 num_epochs=gparams['epochs']
 lr=gparams['lr']
 lambd=gparams['lambda']
@@ -132,11 +165,6 @@ optimizer = torch.optim.Adam(params=DeepONet.parameters(), lr=lr)
 loss_func=nn.MSELoss()
 
 
-## guad pts and weights
-integration_order = 100
-Q = CollisionOperator1D(integration_order)
-grid = torch.DoubleTensor(Q.get_quad_pts()).reshape(-1,1).cuda()
-quad_w = torch.DoubleTensor(Q.get_quad_weights()).cuda()
 
 list_train_loss_deeponet=[]
 list_train_loss_ortho=[]
@@ -157,17 +185,18 @@ for epoch in tqdm(range(1,num_epochs+1)):
         x, y = x.cuda(), y.cuda()
 
         # take a step
-        output=DeepONet(grid,x)
+        output=DeepONet(x,grid)
         loss_deeponet=loss_func(output.squeeze(), y)
         
-        loss_ortho=torch.zeros(1).cuda()
+        loss_ortho=0.0
         basis=DeepONet.trunk_list(grid).transpose(0,1)
-        basis=torch.cat((basis,torch.ones(1,basis.shape[-1]).cuda()))
-        for i in range(w_b+1):
-            for j in range(w_b+1):
+        if not (gparams['use_bias']=='no' and gparams['use_gram']==False):
+            basis=torch.cat((basis,torch.ones(1,basis.shape[-1]).cuda()))
+        for i in range(n_basis):
+            for j in range(n_basis):
                 if j!=i:
                     loss_ortho+=torch.abs(torch.sum(basis[i]*basis[j]*quad_w))
-        loss_ortho/=(w_b*(w_b+1))
+        loss_ortho/=(n_basis*(n_basis-1))
         
         loss=loss_deeponet+lambd*loss_ortho
 
@@ -176,7 +205,7 @@ for epoch in tqdm(range(1,num_epochs+1)):
         optimizer.step()
         with torch.no_grad():
             DeepONet.eval()
-            output=DeepONet(grid,x)
+            output=DeepONet(x,grid)
             Q_rel_error=rel_L2_error(output.squeeze(),y).detach().cpu()
         # update history
         num_batch=x.shape[0]
@@ -194,7 +223,7 @@ for epoch in tqdm(range(1,num_epochs+1)):
     list_train_loss_ortho.append(round(train_loss_ortho,8))
     list_train_Q_rel_error.append(train_Q_rel_error)
     
-    if epoch%10000==0:
+    if epoch%1000==0:
         # Test
         with torch.no_grad():
             DeepONet.eval()
@@ -202,7 +231,7 @@ for epoch in tqdm(range(1,num_epochs+1)):
                 x, y = x.cuda(), y.cuda()
                 with torch.no_grad():
                     DeepONet.eval()
-                    output=DeepONet(grid,x)
+                    output=DeepONet(x,grid)
                     Q_rel_error=rel_L2_error(output.squeeze(),y).detach().cpu()
                 num_batch=x.shape[0]
                 test_Q_rel_error += torch.sum(Q_rel_error)
